067b7276844439de65e091ac8ff6c5c9d51df9da
Artyom Shaitor <artyomshaitor@gmail.com>
2024-02-10 15:10:00 +0100

    Add deleting of habit

--- screens/HabitDetailsScreen.tsx
+++ screens/HabitDetailsScreen.tsx
@@ -1,10 +1,11 @@
-import { SettingsInput } from "@/components/SettingsInput";
+import { useAlert } from "@/hooks/useAlert";
 import { useDummyHabbits } from "@/hooks/useDummyHabits";
 import { useNavigation } from "@/hooks/useNavigation";
 import { Routes } from "@/types/Routes";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { TrashIcon } from "react-native-heroicons/outline";
 
 type HabitDetailsProps = NativeStackScreenProps<Routes, "HabitDetails">;
 
@@ -15,11 +16,35 @@ export const HabitDetailsScreen = ({ route }: HabitDetailsProps) => {
   const { name, id } = habit;
 
   const navigation = useNavigation();
-  const { updateHabit } = useDummyHabbits();
+  const { alert } = useAlert();
+  const { updateHabit, removeHabit } = useDummyHabbits();
   const [input, setInput] = useState(name);
 
   const saveAndClose = () => {
     updateHabit(id, input);
+    navigation.goBack();
+  };
+
+  const removeHabitHandler = async () => {
+    const isDelete = await alert(
+      "Are you sure?",
+      `You're going to delete "${name}"`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          style: "destructive",
+        },
+      ],
+    );
+
+    if (!isDelete) {
+      return;
+    }
+    removeHabit(id);
     navigation.goBack();
   };
 
@@ -34,12 +59,21 @@ export const HabitDetailsScreen = ({ route }: HabitDetailsProps) => {
           onChangeText={setInput}
         />
 
-        <TouchableOpacity
-          onPress={saveAndClose}
-          className="w-full absolute bottom-16 rounded-lg justify-center items-center py-4 bg-orange-500"
-        >
-          <Text className="text-white text-base font-bold">Update</Text>
-        </TouchableOpacity>
+        <View className="w-full absolute bottom-16 gap-y-4">
+          <TouchableOpacity
+            onPress={saveAndClose}
+            className=" rounded-lg justify-center items-center py-4 bg-orange-500"
+          >
+            <Text className="text-white text-base font-bold">Update</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={removeHabitHandler}
+            className="flex-row rounded-lg justify-center items-center py-2 gap-x-1"
+          >
+            <TrashIcon color="rgb(239 68 68)" />
+            <Text className="text-red-500 text-base font-bold">Delete</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
